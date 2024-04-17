553279e9f007faa002fd9fa1e2b71adae115f63c
kritarth17 <akritarth@gmail.com>
2024-04-17 13:03:27 -0400

    added update and delete functionaliy

--- react-job-portal/src/components/HomeCards.jsx
+++ react-job-portal/src/components/HomeCards.jsx
@@ -18,7 +18,7 @@ const HomeCards = () => {
             <p className='mt-2 mb-4'>
               List your job to find the perfect developer for the role
             </p>
-            <Link className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600' to = "/add-jobs"> Add Jobs</Link>
+            <Link className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600' to = "/add-job"> Add Jobs</Link>
           </Card>
         </div>
       </div>
